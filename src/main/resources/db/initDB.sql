6b566a4b832db20291301c23ff6a9e9ae9b406b6
Sergey Ivanov <sergie92@yandex.ru>
2021-04-06 00:20:08 +0300

    HW_03_pt1

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -23,3 +24,16 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id           INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    description  VARCHAR                           NOT NULL,
+    mealdatetime TIMESTAMP           DEFAULT now() NOT NULL,
+    calories     INTEGER                           NOT NULL,
+    user_id      INTEGER                           NOT NULL,
+    CONSTRAINT users_meal UNIQUE (user_id, mealdatetime),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX users_meals ON meals(user_id);
